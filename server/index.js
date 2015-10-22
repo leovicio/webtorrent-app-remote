95ad01d0de3028b7ef75f62a0b60f30a4c4aaf81
André Stein <stivekx@gmail.com>
2015-10-22 18:32:00 +0000

    RReplace express by serve static and remove unused deps

--- server/index.js
+++ server/index.js
@@ -1,43 +1,34 @@
 /**
  * Module dependecies 
  */
-
-var express = require('express')
-var path = require('path')
-var auth = require('http-auth')
-var logger = require('morgan')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
 var WebTorrent = require('webtorrent-hybrid/node_modules/webtorrent')
-var debug = require('debug')('webtorrentapp:server');
-var http = require('http');
+//var debug = require('debug')('webtorrentapp:server')
+var http = require('http')
+var NodeStatic = require( 'node-static' )
 
 /**
- * Express routes 
+ * Initiate levelup database
  */
-var routes = require('./routes/index')
+var levelup = require('levelup')
+var db = levelup(__dirname + '/settings/database')
 
-var app = express()
-var port = normalizePort(process.env.PORT || '3001');
-app.set('port', port);
-var server = http.createServer(app)
+
+/**
+ * Static file serve
+ */
+var file = new NodeStatic.Server( './client', {
+    cache: 3600,
+    gzip: true
+} );
+
+var port = process.env.PORT || '3001';
+var server = http.createServer(function ( request, response ) {
+    request.addListener( 'end', function () {
+        file.serve( request, response );
+    } ).resume();
+})
 
 server.listen(port);
-
-/**
- * Handle Server Events
- */
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Basic Auth (should be removed soon)
- */
-var basic = auth.basic({
-  realm: 'WebTorrent App Area.',
-  file: __dirname + '/users.htpasswd'
-})
-app.use(auth.connect(basic))
 
 /**
  * Socket IO Server 
@@ -46,40 +37,6 @@ var io = require('socket.io')
 var http = require('http')
 
 io = io.listen(server)
-
-/* 
- * Setup express views
- */
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-app.use(cookieParser())
-app.use(express.static(__dirname + '/../client'))
-
-app.use('/', routes)
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
-
-/**
- * Express error handler Event
- */
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500)
-  res.render('error', {
-    message: err.message,
-    error: {}
-  })
-})
 
 /**
  * Start webTorrent Client
@@ -102,65 +59,3 @@ System_info = new System_info()
  * Now require Socket.js Events
  */
 require('./socket.js')(io, Torrent, System_info, Tracker)
-
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-    console.log(bind)
-  debug('Listening on ' + bind);
-}
